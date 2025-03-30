9ca667966ae4d97277405dd5a37d9ba6e0f754c9
Pablo Aristizabal <radabeca23@gmail.com>
2025-03-30 09:41:33 -0500

    eliminar productos del carrito

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -8,6 +8,11 @@ import './styles.css'
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
 
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id !== id)
+    context.setCartProducts(filteredProducts)
+  }
+
   return (
     <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed top-20 bg-white right-0 border border-black rounded-lg`}>
         <div className='flex justify-between items-center px-6'>
@@ -24,9 +29,11 @@ const CheckoutSideMenu = () => {
             context.cartProducts.map(product => (
                 <OrderCard
                   key={product.id}
+                  id={product.id}
                   title={product.title}
                   imageUrl={product.images}
                   price={product.price}
+                  handleDelete={handleDelete}
                 />
             ))
           }
